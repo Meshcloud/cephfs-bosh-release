20b627d3a1818ce0097949a08ee8177dcefd9b78
Jannik Heyl <Jannik.Heyl@googlemail.com>
2018-01-10 14:10:24 +0100

    fixed two bugs that produced an error in the errand

--- jobs/get_credentials/templates/run.sh
+++ jobs/get_credentials/templates/run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash 
 set -x
 CLUSTER="/root/ceph-cluster/"
-RANDOM=`shuf -i 2000-65000 -n 1`
+RAN=`shuf -i 2000-65000 -n 1`
 
 set -e
 pushd $CLUSTER
 
-if [ ! -e "$CLUSRER/ceph.conf" ]; then
+if [  -e "$CLUSRER/ceph.conf" ]; then
 	ceph osd pool create "cephfs_data_$RANDOM" 128
-	ceph osd pool create "cephfs_metadata_$RANDOM" 128
-	ceph fs new "cephfs_$RANDOM " "cephfs_data_$RANDOM" "cephfs_data_$RANDOM"
+	ceph osd pool create "cephfs_metadata_$RAN" 128
+	ceph fs new "cephfs_$RAN " "cephfs_data_$RAN" "cephfs_data_$RAN"
 
 	if [[ $(ceph mds stat | grep "active") = '' ]]
 	then
@@ -17,7 +17,7 @@ if [ ! -e "$CLUSRER/ceph.conf" ]; then
         exit 1
 	fi
 
-	ceph auth add client.test mon 'allow r' osd 'allow rw pool="cephfs_data_$RANDOM" allow rw pool="cephfs_data_$RANDOM"' 
+	ceph auth add client.test mon 'allow r' osd 'allow rw pool="cephfs_data_$RAN" allow rw pool="cephfs_data_$RAN"' 
 
 fi
 exit 0
--- jobs/smoketest/templates/run.sh
+++ jobs/smoketest/templates/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
-sudo apt install ceph-common ceph-fs-common -y
+FIRSTMON=<%= link('ceph-storage').instances.first.adress %>
+apt-get install ceph-common ceph-fs-common -y
+mkdir -p /mnt/mycephfs
+mount -t ceph $FIRSTMON /mnt/mycephfs -o name=testuser,secret=AQDrWzJae1DGCxAAXGvPgKptRBAIGM3bxztrtw== 
