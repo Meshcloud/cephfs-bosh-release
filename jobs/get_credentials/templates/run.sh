7f3b9bccc0ab3eafe57e4699ef72ec3ea89bf037
Jannik Heyl <Jannik.Heyl@googlemail.com>
2018-01-11 18:17:26 +0100

    changed ssh config

--- jobs/get_credentials/templates/run.sh
+++ jobs/get_credentials/templates/run.sh
@@ -1,24 +1,22 @@
 #!/bin/bash 
 set -x
+SSH="/root/.ssh/"
 FIRSTMON=<%= link('ceph-storage').instances.first.address %>
-mv /var/vcap/jobs/get_credentials/config/certs/config/* /root/.ssh/
-ssh $FIRSTMON 'bash -s' <EOF
+mkdir -p $SSH
+mv /var/vcap/jobs/get_credentials/config/certs/* $SSH
+chmod 600 $SSH/priv.rsa
+ssh $FIRSTMON 'bash -s' <<EOF
 CLUSTER="/root/ceph-cluster/"
 RAN=`shuf -i 2000-65000 -n 1`
-
-set -e
 pushd $CLUSTER
-
 ceph osd pool create "cephfs_data_$RAN" 128
 ceph osd pool create "cephfs_metadata_$RAN" 128
 ceph fs new "cephfs_$RAN " "cephfs_data_$RAN" "cephfs_data_$RAN"
-
 if [[ $(ceph mds stat | grep "active") = '' ]]
 then
 	echo "Meta Data Service is not active"
 	exit 1
 fi
-
 ceph auth add client.test mon 'allow r' osd 'allow rw pool="cephfs_data_$RAN" allow rw pool="cephfs_data_$RAN"' 
 ceph auth list
 exit 0
