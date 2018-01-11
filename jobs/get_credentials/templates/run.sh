049380fed71d924d6b2a5f3a6df1ac50476b59a0
Jannik Heyl <Jannik.Heyl@googlemail.com>
2018-01-11 12:04:42 +0100

    FIxed a Typo

--- jobs/get_credentials/templates/run.sh
+++ jobs/get_credentials/templates/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 set -x
-FIRSTMON=<%= link('ceph-storage').instances.first.adress %>
+FIRSTMON=<%= link('ceph-storage').instances.first.address %>
 
 ssh $FIRSTMON 'bash -s' < EOF
 CLUSTER="/root/ceph-cluster/"
@@ -9,7 +9,7 @@ RAN=`shuf -i 2000-65000 -n 1`
 set -e
 pushd $CLUSTER
 
-ceph osd pool create "cephfs_data_$RANDOM" 128
+ceph osd pool create "cephfs_data_$RAN" 128
 ceph osd pool create "cephfs_metadata_$RAN" 128
 ceph fs new "cephfs_$RAN " "cephfs_data_$RAN" "cephfs_data_$RAN"
 
