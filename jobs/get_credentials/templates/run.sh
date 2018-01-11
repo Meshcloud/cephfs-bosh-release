76ad16c163731d3c009788c4980941b62e539bae
Jannik Heyl <Jannik.Heyl@googlemail.com>
2018-01-11 11:53:38 +0100

    changed errand to run via SSH

--- jobs/get_credentials/templates/run.sh
+++ jobs/get_credentials/templates/run.sh
@@ -1,23 +1,25 @@
 #!/bin/bash 
 set -x
+FIRSTMON=<%= link('ceph-storage').instances.first.adress %>
+
+ssh $FIRSTMON 'bash -s' < EOF
 CLUSTER="/root/ceph-cluster/"
 RAN=`shuf -i 2000-65000 -n 1`
 
 set -e
 pushd $CLUSTER
 
-if [  -e "$CLUSRER/ceph.conf" ]; then
-	ceph osd pool create "cephfs_data_$RANDOM" 128
-	ceph osd pool create "cephfs_metadata_$RAN" 128
-	ceph fs new "cephfs_$RAN " "cephfs_data_$RAN" "cephfs_data_$RAN"
+ceph osd pool create "cephfs_data_$RANDOM" 128
+ceph osd pool create "cephfs_metadata_$RAN" 128
+ceph fs new "cephfs_$RAN " "cephfs_data_$RAN" "cephfs_data_$RAN"
 
-	if [[ $(ceph mds stat | grep "active") = '' ]]
-	then
-		echo "Meta Data Service is not active"
-        exit 1
-	fi
-
-	ceph auth add client.test mon 'allow r' osd 'allow rw pool="cephfs_data_$RAN" allow rw pool="cephfs_data_$RAN"' 
-
+if [[ $(ceph mds stat | grep "active") = '' ]]
+then
+	echo "Meta Data Service is not active"
+	exit 1
 fi
+
+ceph auth add client.test mon 'allow r' osd 'allow rw pool="cephfs_data_$RAN" allow rw pool="cephfs_data_$RAN"' 
+ceph auth list
 exit 0
+EOF
