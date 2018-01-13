d2046977a86fc5001b60aeefe6893218898d8891
Jannik Heyl <Jannik.Heyl@googlemail.com>
2018-01-13 13:28:10 +0100

    Added non active sources for a different way to activate cephx

--- jobs/smoketest/templates/run.sh
+++ jobs/smoketest/templates/run.sh
@@ -2,4 +2,4 @@
 FIRSTMON=<%= link('ceph-storage').instances.first.adress %>
 apt-get install ceph-common ceph-fs-common -y
 mkdir -p /mnt/mycephfs
-mount -t ceph $FIRSTMON /mnt/mycephfs -o name=testuser,secret=AQDrWzJae1DGCxAAXGvPgKptRBAIGM3bxztrtw== 
+mount -t ceph $FIRSTMON:/ /mnt/mycephfs -o name=testuser,secret=AQDrWzJae1DGCxAAXGvPgKptRBAIGM3bxztrtw== 
